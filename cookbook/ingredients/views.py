f40ba3d92a157ddaec8b349ea346b94d9bd2576b
Yeganathan S <yeganathan18@gmail.com>
2021-01-20 21:52:38 +0530

    first commit

---
+++ cookbook/ingredients/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.contrib import admin
+from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
+
+from graphene_django.views import GraphQLView
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True))),
+]
